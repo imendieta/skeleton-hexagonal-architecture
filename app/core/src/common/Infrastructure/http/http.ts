07fa5bf926a9878e332d675f79b7ef91073fbde7
imendieta <inaki.mendieta.aurtenetxe@gmail.com>
2022-04-09 13:35:36 +0200

    fix/http-ploc files

--- app/core/src/common/Infrastructure/http/http.ts
+++ app/core/src/common/Infrastructure/http/http.ts
@@ -2,7 +2,7 @@ const headers = {
     'Content-Type': 'application/json'
 }
 
-export const get = async <T>(url:string) => {
+const get = async <T>(url:string) => {
     const response = await fetch(url, {
         method: 'GET',
         headers,
@@ -10,7 +10,7 @@ export const get = async <T>(url:string) => {
     return await response.json() as T;
 }
 
-export const post = async <T>(url:string, body:any) => {
+const post = async <T>(url:string, body:any) => {
     const response = await fetch(url, {
         method: 'POST',
         headers,
@@ -19,7 +19,7 @@ export const post = async <T>(url:string, body:any) => {
     return await response.json() as T;
 }
 
-export const put = async <T>(url:string, body:any) => {
+const put = async <T>(url:string, body:any) => {
     const response = await fetch(url, {
         method: 'PUT',
         headers,
@@ -28,10 +28,17 @@ export const put = async <T>(url:string, body:any) => {
     return await response.json() as T;
 };
 
-export const _delete = async <T>(url:string) => {
+const _delete = async <T>(url:string) => {
     const response = await fetch(url, {
         method: 'DELETE',
         headers,
     });
     return await response.json() as T;
 }
+
+export const http = {
+    get,
+    post,
+    put,
+    _delete,
+};
